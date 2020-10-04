2d4b7ffcd4d8f3cc9f6445d34424f156bcd02372
vierundsechzig <nickluisliu@outlook.com>
2020-10-04 18:51:26 +0800

    Account database finished

--- app/src/main/java/com/example/oohoobkk/AccountHelper.java
+++ app/src/main/java/com/example/oohoobkk/AccountHelper.java
@@ -10,15 +10,17 @@ import android.widget.Toast;
 public class AccountHelper extends SQLiteOpenHelper {
     public static final String CREATE = "create table Account(id integer primary key autoincrement, " +
             "username text, password text, question integer, answer text, " +
-            "patternenabled integer, pattern integer)";
+            "patternenabled integer, pattern text)";
     private Context aContext;
 
+    // 当前数据库版本为1，请传入1
     public AccountHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
         aContext = context;
     }
 
-    public void signup(String username, String password, int question, String answer, int patternEnabled, int pattern) {
+    // 添加一条记录到账号数据库中，注册时调用
+    public void signup(String username, String password, int question, String answer, int patternEnabled, String pattern) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("username", username);
@@ -30,6 +32,37 @@ public class AccountHelper extends SQLiteOpenHelper {
         db.insert("Account", null, values);
     }
 
+    // 查询指定用户名的数据
+    public Cursor query(String username) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query("Account", null, "username=?", new String[]{username}, null, null, null);
+    }
+
+    // 修改指定用户名的信息。该方法不会检查username指定的字段是否存在，需先用query方法检查。
+    // 若不想修改某些字段，则password, answer, pattern字段传入null，question字段传入0，patternEnabled字段传入除0和1以外的值
+    public void update(String username, String password, int question, String answer, int patternEnabled, String pattern) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        if (password != null)
+            values.put("password", password);
+        if (question != 0)
+            values.put("question", question);
+        if (answer != null)
+            values.put("answer", answer);
+        if (patternEnabled == 0 || patternEnabled == 1)
+            values.put("patternenabled", patternEnabled);
+        if (pattern != null)
+            values.put("pattern", pattern);
+        db.update("Account", values, "username = ?", new String[] {username});
+    }
+
+    // 注销账户
+    public void delete(String username) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete("Account", "username = ?", new String[] {username});
+    }
+
+    // 列出数据库中所有数据，调试时用
     public Cursor listall() {
         SQLiteDatabase db = this.getReadableDatabase();
         return db.query("Account", null, null, null, null, null, null);
--- app/src/main/java/com/example/oohoobkk/MainActivity.java
+++ app/src/main/java/com/example/oohoobkk/MainActivity.java
@@ -1,46 +1,67 @@
 package com.example.oohoobkk;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private AccountHelper accountHelper;
     EditText pat;
-    TextView result;
     EditText tUsrn;
     EditText tPwd;
     Spinner sQuest;
     EditText tAns;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch paten;
+    RecyclerView list;
+    List<String> uList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         accountHelper = new AccountHelper(this, "account.db", null, 1);
-        pat = (EditText) findViewById(R.id.txt_pat);
-        result = (TextView) findViewById(R.id.txt_res);
-        Button bAdd = (Button) findViewById(R.id.btn_add);
-        Button bList = (Button) findViewById(R.id.btn_list);
-        tUsrn = (EditText) findViewById(R.id.txt_usrn);
-        tPwd = (EditText) findViewById(R.id.txt_pwd);
-        sQuest = (Spinner) findViewById(R.id.spinner_q);
-        tAns = (EditText) findViewById(R.id.txt_ans);
-        paten = (Switch) findViewById(R.id.switch_p);
+        pat = findViewById(R.id.txt_pat);
+        Button bAdd = findViewById(R.id.btn_add);
+        Button bList = findViewById(R.id.btn_list);
+        Button bDelete = findViewById(R.id.btn_delete);
+        Button bUpdate = findViewById(R.id.btn_update);
+        tUsrn = findViewById(R.id.txt_usrn);
+        tPwd = findViewById(R.id.txt_pwd);
+        sQuest = findViewById(R.id.spinner_q);
+        tAns = findViewById(R.id.txt_ans);
+        paten = findViewById(R.id.switch_p);
+        list = findViewById(R.id.lst_all);
+        uList = new ArrayList<>();
+        updateAccounts();
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(new AccountAdapter(uList));
+        list.setItemAnimator(new DefaultItemAnimator());
         paten.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -51,33 +72,74 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 accountHelper.signup(tUsrn.getText().toString(),
-                        tPwd.getText().toString(),
+                        MainActivity.encrypt(tPwd.getText().toString()),
                         Integer.parseInt(sQuest.getSelectedItem().toString()),
                         tAns.getText().toString(),
                         paten.isChecked() ? 1 : 0,
-                        pat.isEnabled() ? Integer.parseInt(pat.getText().toString()) : 0
+                        MainActivity.encrypt(pat.getText().toString())
                         );
             }
         });
         bList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                StringBuilder s = new StringBuilder();
-                Cursor c = accountHelper.listall();
-                if (c.moveToFirst()) {
-                    do {
-                        String usr = c.getString(c.getColumnIndex("username"));
-                        String pwd = c.getString(c.getColumnIndex("password"));
-                        int qst = c.getInt(c.getColumnIndex("question"));
-                        String ans = c.getString(c.getColumnIndex("answer"));
-                        int ptnen = c.getInt(c.getColumnIndex("patternenabled"));
-                        int ptn = c.getInt(c.getColumnIndex("pattern"));
-                        s.append(usr).append(" ").append(pwd).append(" ").append(qst).append(" ");
-                        s.append(ans).append(" ").append(ptnen).append(" ").append(ptn).append("\n");
-                    } while (c.moveToNext());
-                    result.setText(s.toString());
-                }
+                MainActivity.this.updateAccounts();
+                Objects.requireNonNull(list.getAdapter()).notifyDataSetChanged();
+            }
+        });
+        bDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                accountHelper.delete(tUsrn.getText().toString());
+            }
+        });
+        bUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                accountHelper.update(tUsrn.getText().toString(),
+                        MainActivity.encrypt(tPwd.getText().toString()),
+                        Integer.parseInt(sQuest.getSelectedItem().toString()),
+                        tAns.getText().toString(),
+                        paten.isChecked() ? 1 : 0,
+                        MainActivity.encrypt(pat.getText().toString())
+                        );
             }
         });
     }
+
+    public void updateAccounts() {
+        Cursor c = accountHelper.listall();
+        uList = new ArrayList<>();
+        if (c.moveToFirst()) {
+            do {
+                String usr = c.getString(c.getColumnIndex("username"));
+                String pwd = c.getString(c.getColumnIndex("password"));
+                int qst = c.getInt(c.getColumnIndex("question"));
+                String ans = c.getString(c.getColumnIndex("answer"));
+                int pte = c.getInt(c.getColumnIndex("patternenabled"));
+                String ptn = c.getString(c.getColumnIndex("pattern"));
+                List<String> sl = new ArrayList<>();
+                sl.add(usr);
+                sl.add(MainActivity.encrypt(pwd));
+                sl.add(String.valueOf(qst));
+                sl.add(ans);
+                sl.add(String.valueOf(pte));
+                sl.add(MainActivity.encrypt(pwd));
+                uList.add(sl.toString());
+            } while (c.moveToNext());
+        }
+        list.setAdapter(new AccountAdapter(uList));
+    }
+
+    public static String encrypt(String str) {
+        byte[] encrypted = null;
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("md5");
+            encrypted = md5.digest(str.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return new BigInteger(1, encrypted).toString(16);
+    }
+
 }
